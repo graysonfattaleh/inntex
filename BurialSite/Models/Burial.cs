61c8be9ce9cdc36777e2ed7a940818f2a038f332
Max Haines <Pjhaines98@outlook.com>
2021-04-08 18:20:00 -0600

    Max Models

--- BurialSite/Models/Burial.cs
+++ BurialSite/Models/Burial.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+#nullable enable
 
 namespace BurialSite.Models
 {
@@ -33,13 +34,13 @@ namespace BurialSite.Models
 
         public decimal? Depth { get; set; }
 
-        public string Preservation_State { get; set; }
+        public string? Preservation_State { get; set; }
 
-        public string Burial_Icon { get; set; }
+        public string? Burial_Icon { get; set; }
 
-        public string Burial_Icon2 { get; set; }
+        public string? Burial_Icon2 { get; set; }
 
-        public string Sex_Method { get; set; }
+        public string? Sex_Method { get; set; }
 
         public char? Sex { get; set; }
 
@@ -51,13 +52,13 @@ namespace BurialSite.Models
 
         public decimal? Age_At_Death { get; set; }
 
-        public string Age_Method { get; set; }
+        public string? Age_Method { get; set; }
 
-        public string Hair_Color { get; set; }
+        public string? Hair_Color { get; set; }
 
         public bool? Sample_Collected { get; set; }
 
-        public string Burial_Situation { get; set; }
+        public string? Burial_Situation { get; set; }
 
         public decimal? Length_Of_Remains { get; set; }
 
@@ -83,9 +84,9 @@ namespace BurialSite.Models
 
         public decimal? Humerus_Head { get; set; }
 
-        public string Osteophytosis { get; set; }
+        public string? Osteophytosis { get; set; }
 
-        public string Pubic_Symphysis { get; set; }
+        public string? Pubic_Symphysis { get; set; }
 
         public decimal? Femur_Length { get; set; }
 
@@ -107,7 +108,7 @@ namespace BurialSite.Models
         // Range of 0-3
         public int? Zygomatic_Crest { get; set; }
         // Range of 0-3
-        public string Cranial_Suture { get; set; }
+        public string? Cranial_Suture { get; set; }
         // Closed, Mostly Closed, or Closed
 
         public decimal? Maximum_Cranial_Length { get; set; }
@@ -128,7 +129,7 @@ namespace BurialSite.Models
 
         public decimal? Interorbital_Breadth { get; set; }
 
-        public string Artifacts_Description { get; set; }
+        public string? Artifacts_Description { get; set; }
 
         public int? Preservation_Index { get; set; }
         // Romman Numerals
@@ -143,22 +144,22 @@ namespace BurialSite.Models
 
         public bool? Textile_Taken { get; set; }
 
-        public string Description_Of_Taken { get; set; }
+        public string? Description_Of_Taken { get; set; }
 
         public bool? Artifact_Found { get; set; }
 
         public decimal? Estimate_Living_Stature { get; set; }
         public int? Tooth_Attrition { get; set; }
         // Roman Numerals
-        public string Tooth_Eruption { get; set; }
-        public string Pathology_Anomalies { get; set; }
-        public string Epiphyseal_Union { get; set; }
+        public string? Tooth_Eruption { get; set; }
+        public string? Pathology_Anomalies { get; set; }
+        public string? Epiphyseal_Union { get; set; }
         public DateTime? Date_Excavated { get; set; }
         // specifc day, month, and year
         public bool? Head_Direction { get; set; }
         // True = West, False = East
-        public string Area { get; set; }
-        public string Artifact_Description { get; set; }
+        public string? Area { get; set; }
+        public string? Artifact_Description { get; set; }
         public bool? Artifacts_Found { get; set; }
         public DateTime? Year_On_Skull { get; set; }
         // datetime.year
@@ -166,32 +167,32 @@ namespace BurialSite.Models
         // datetime.month
         public DateTime? Day_On_Skull { get; set; }
         // DateTime.day
-        public string Field_Book { get; set; }
+        public string? Field_Book { get; set; }
         public int? Field_Book_Page_Number { get; set; }
-        public string Initials_Of_Data_Entry_Expert { get; set; }
-        public string Initials_Of_Data_Entry_Checker { get; set; }
+        public string? Initials_Of_Data_Entry_Expert { get; set; }
+        public string? Initials_Of_Data_Entry_Checker { get; set; }
         public bool? BYU_Sample { get; set; }
         public DateTime? Body_Analysis { get; set; }
         // datetime.year
         public bool? Skull_At_Magazine { get; set; }
         public bool? Postcrania_At_Magazine { get; set; }
-        public string Age { get; set; }
+        public string? Age { get; set; }
         // This includes words
-        public string Rack_And_Shelf { get; set; }
+        public string? Rack_And_Shelf { get; set; }
         public bool? Skull_Trauma { get; set; }
         public bool? Postcrania_Trauma { get; set; }
         public bool? Cribra_Orbitala { get; set; }
-        public string Porotic_Hyperostosis { get; set; }
+        public string? Porotic_Hyperostosis { get; set; }
         // would be bool but also includes 'BM'
-        public string Porotic_Hyperostosis_Locations { get; set; }
+        public string? Porotic_Hyperostosis_Locations { get; set; }
         public bool? Metopic_Suture { get; set; }
         public bool? Button_Osteoma { get; set; }
-        public string Osteology_Unknown_Comment { get; set; }
+        public string? Osteology_Unknown_Comment { get; set; }
         public bool? Temporal_Mandibular_Joint_Osteoarthritis { get; set; }
         public bool? Linear_Hypoplasia_Enamel { get; set; }
         public int? Tomb { get; set; }
         public decimal? Length { get; set; }
-        public string Burial_Preservation { get; set; }
+        public string? Burial_Preservation { get; set; }
         public char? Burial_Wrapping { get; set; }
         //just uses B,H,S,U,W 
         public char? Burial_Adult_Child { get; set; }
@@ -202,17 +203,17 @@ namespace BurialSite.Models
         public decimal? Length_In_Meters { get; set; }
         public decimal? Length_In_Centimeters { get; set; }
         public decimal? Length_In_Millimeters { get; set; }
-        public string Goods { get; set; }
-        public string Cluster { get; set; }
+        public string? Goods { get; set; }
+        public string? Cluster { get; set; }
         public bool? Face_Bundle { get; set; }
         public int? Rank { get; set; }
         public int? Tube_Number { get; set; }
-        public string Burial_Description { get; set; }
+        public string? Burial_Description { get; set; }
         public int? Foci { get; set; }
         //only goes from 1-5
         public int? C14_Sample { get; set; }
-        public string Location { get; set; }
-        public string Questions { get; set; }
+        public string? Location { get; set; }
+        public string? Questions { get; set; }
         public int? Conventional_14C_Age_BP { get; set; }
         public int? C14_Calendar_Date { get; set; }
         // might be negative, datetime.year
@@ -224,13 +225,13 @@ namespace BurialSite.Models
         // datetime.year
         public int? Calibrated_95_Calendar_Date_Avg { get; set; }
         // you will have to include BC in it, datetime.year
-        public string Category { get; set; }
+        public string? Category { get; set; }
         // options are Deepest East, Deepest West, Hill B, One Meter Deep
         public int? Bag { get; set; }
         public DateTime? Date { get; set; }
         // this will be a year only
         public bool? Previously_Sampled { get; set; }
-        public string Initials { get; set; }
+        public string? Initials { get; set; }
         // options are MBB, GM, AWA, and CIE for now
     }
 }
---
+++ BurialSite/Models/BurialLocation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+#nullable enable
+
+namespace BurialSite.Models
+{
+    public class BurialLocation
+    {
+        [Key]
+        [Required]
+        public int BurLocID { get; set; }
+        [Required]
+        public int BurialID { get; set; }
+        [Required]
+        public string? Dig_Site_Name { get; set; }
+        public int? N_S_Grid_Site_Upper { get; set; }
+        public int? N_S_Grid_Site_Lower { get; set; }
+        public int? E_W_Grid_Site_Upper { get; set; }
+        public int? E_W_Grid_Site_Lower { get; set; }
+        public string? Area { get; set; } = "";
+
+
+
+
+
+
+
+    }
+}
