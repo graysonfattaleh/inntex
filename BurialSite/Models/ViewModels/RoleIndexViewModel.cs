3c8e70936f2d18308c8053d62cab7cbed35823c7
Squarerootofpi <53194917+Squarerootofpi@users.noreply.github.com>
2021-04-10 13:41:22 -0600

    Authentication tie in 1 steed Sat Morn

---
+++ BurialSite/Models/ViewModels/RoleIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BurialSite.Models.ViewModels
+{
+    public class RoleIndexViewModel
+    {   
+        public List<Role> Roles { get; set; }
+        public List<ResearchUser> Users { get; set; }
+    }
+}
