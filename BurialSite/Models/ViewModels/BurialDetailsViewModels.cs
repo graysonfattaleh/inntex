676a204c783046bab6877dc1de8560e83b9b5461
Squarerootofpi <53194917+Squarerootofpi@users.noreply.github.com>
2021-04-13 19:44:58 -0600

    Custom Fields Completed

--- BurialSite/Models/ViewModels/BurialDetailsViewModels.cs
+++ BurialSite/Models/ViewModels/BurialDetailsViewModels.cs
@@ -14,11 +14,22 @@ namespace BurialSite.Models.ViewModels
             burial = _context.Burials.Include(b => b.BurialLocation).Where(b => b.BurialID == BurialId).FirstOrDefault(); ;
             BurialPhotos = _context.FileUrls.Where(b => b.Burial.BurialID == BurialId).ToList();
             Notes = _context.Notes.Where(n => n.BurialID == BurialId).ToList();
-
+            CustomFields = getOnetoOneDict();
         }
 
         public Burial burial { get; set; }
         public List<FileUrl> BurialPhotos {get;set;}
         public List<Notes> Notes { get; set; }
+        public Dictionary<OneToOneField, OneToOneValue> CustomFields { get; set; }
+
+        private Dictionary<OneToOneField, OneToOneValue> getOnetoOneDict()
+        {
+            Dictionary<OneToOneField, OneToOneValue> returndict = new Dictionary<OneToOneField, OneToOneValue>();
+            foreach (OneToOneField field in _context.OneToOneFields.ToList())
+            {
+                returndict.Add( field, _context.OneToOneValues.Where(val => val.BurialId == burial.BurialID && val.OneToOneFieldId == field.OneToOneFieldId).FirstOrDefault()) ;
+            }
+            return returndict;
+        }
     }
 }
--- BurialSite/Models/ViewModels/EditBurialViewModel.cs
+++ BurialSite/Models/ViewModels/EditBurialViewModel.cs
@@ -23,6 +23,7 @@ namespace BurialSite.Models.ViewModels
             CranialStructureList = CranialOptions;
             _context = context;
             configureLocations();
+            CustomFields = getOnetoOneDict();
         }
         public IEnumerable<SelectListItem> BasilList { get; set; }
         public IEnumerable<SelectListItem> CranialStructureList { get; set; }
@@ -31,18 +32,40 @@ namespace BurialSite.Models.ViewModels
         public Dictionary<string, int> LocationStrings { get; set; }
         public Dictionary<int, string> AreaStrings { get; set; } = Area.Areas;
         List<BurialLocation> Locations { get; set; }
+
+        public Dictionary<OneToOneField, OneToOneValue> CustomFields { get; set; }
+
+        private Dictionary<OneToOneField, OneToOneValue> getOnetoOneDict()
+        {
+            Dictionary<OneToOneField, OneToOneValue> returndict = new Dictionary<OneToOneField, OneToOneValue>();
+            foreach (OneToOneField field in _context.OneToOneFields.ToList())
+            {
+                OneToOneValue? that;
+                try
+                {
+                    that = _context.OneToOneValues.Where(val => val.BurialId == Burial.BurialID && val.OneToOneFieldId == field.OneToOneFieldId).FirstOrDefault();
+                }
+                catch (Exception)
+                {
+                    that = null;
+                }
+                returndict.Add(field, that);
+            }
+            return returndict;
+        }
         private void configureLocations()
         {
             Locations = _context.BurialLocations.ToList();
             Dictionary<string, int> locationvalues = new Dictionary<string, int>();
 
             //locationvalues["All"] = -1;//TODO FIX THIS??
-            foreach (BurialLocation bl in Locations)
-            {
+            foreach (BurialLocation bl in Locations) { 
+            
                 if (bl.Area == null || bl.Area == "")  
                 {
                     bl.Area = "1";
                 }
+            
                 string area = Area.Areas[Int32.Parse(bl.Area)];
                 string namestring = $"{bl.Dig_Site_Name} NS: {bl.N_S_Grid_Site_Lower}/ {bl.N_S_Grid_Site_Upper} EW: {bl.E_W_Grid_Site_Lower}/ {bl.E_W_Grid_Site_Upper} / Area: {area}"; 
                 locationvalues[namestring] = bl.BurialLocationId;
